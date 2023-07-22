f01f0b02f063d685a181f5950e47291f642137e5
Aaron Romeo <aaron@aaronromeo.com>
2023-07-22 19:25:35 +0100

    Initial add of the rfid-music project

---
+++ README.md
@@ -0,0 +1,15 @@
+# RFID-Music
+
+## Getting Started
+
+### Prerequisites
+_Assumes `cmus` is installed_
+
+* Create a venv
+    * `python -m venv env`
+* Install the `requirements.txt`
+    * `pip install -r requirements.txt`
+
+
+
+
---
+++ utils/rfid-folder-creator.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+import os
+import RPi.GPIO as GPIO
+from mfrc522 import SimpleMFRC522
+
+ROOT_DIR = "music"
+
+reader = SimpleMFRC522()
+
+try:
+        print("Ready to read")
+        id, _text = reader.read()
+        path = os.path.join(ROOT_DIR, "card-%s" % str(id))
+        os.mkdir(path)
+        print("Create %s" % path)
+finally:
+        GPIO.cleanup()
+
