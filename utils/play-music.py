fb316e83dbe5bb0cac176765c68c8ace852bad2b
Aaron Romeo <aaron@aaronromeo.com>
2023-10-19 21:42:09 -0400

    Don't remember why I made this change.

--- utils/play-music.py
+++ utils/play-music.py
@@ -4,32 +4,27 @@ import os
 import RPi.GPIO as GPIO
 from mfrc522 import SimpleMFRC522
 
+# cmus-remote -s -c -f ~/workspace/pyrfid-jukebox/music/card-1061668068229/*
+
 ROOT_DIR = "music"
-CMS_CLEAR_PLAYLIST = "cmus-remote -c -q"
-CMS_CLEAR_QUEUE = "cmus-remote -c -p"
-CMS_ADD_TO_QUEUE = "cmus-remote -C \"add -q {}\""
-CMS_NEXT = "cmus-remote -n"
-CMS_PLAY = "cmus-remote -p"
+CMS_PLAY_FOLDER = "cmus-remote -s -c -f {}"
 
 reader = SimpleMFRC522()
 
 while True:
     try:
-            print("Ready to read")
-            id, _text = reader.read()
-            path = os.path.abspath(
-                    os.path.join(ROOT_DIR, "card-%s" % str(id))
-            )
-            cmd = " && ".join([
-            CMS_CLEAR_PLAYLIST, CMS_CLEAR_PLAYLIST, CMS_ADD_TO_QUEUE.format(path), CMS_NEXT, CMS_PLAY     
-            ])
-            print(cmd)
-            if os.system(cmd) == 0:
-                print("Playing %s" % path)
-            else:
-                print("Error with %s" % path)
+        print("Ready to read")
+        id, _text = reader.read()
+        path = os.path.abspath(
+            os.path.join(ROOT_DIR, "card-%s" % str(id))
+        )
+        cmd = CMS_PLAY_FOLDER.format(path)
+        print(cmd)
+        if os.system(cmd) == 0:
+            print("Playing %s" % path)
+        else:
+            print("Error with %s" % path)
     except Exception:
-            print("Error with %s" % path)      
+        print("Error with %s" % path)
     finally:
-            GPIO.cleanup()
-
+        GPIO.cleanup()
