fe9f3c00483136966d8a9eff193e8e8025b621ea
Aaron Romeo <aaron@aaronromeo.com>
2023-10-20 02:44:37 +0100

    Testing...

--- utils/button-test.py
+++ utils/button-test.py
@@ -1,19 +1,19 @@
 import RPi.GPIO as GPIO # Import Raspberry Pi GPIO library
 
-GPIO.setwarnings(False) # Ignore warning for now
+# GPIO.setwarnings(False) # Ignore warning for now
 GPIO.setmode(GPIO.BOARD) # Use physical pin numbering
-GPIO.setup(10, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 10 to be an input pin and set initial value to be pulled low (off)
+GPIO.setup(37, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 10 to be an input pin and set initial value to be pulled low (off)
 
 def button_callback(channel):
-    print("Button 10 was pushed!")
+    print("Button 37 was pushed!")
 
 try:
-    # while True: # Run forever
-    #     if GPIO.input(10) == GPIO.HIGH:
-    #         print("Button 10 was pushed!")
-    GPIO.add_event_detect(10,GPIO.RISING,callback=button_callback) # Setup event on pin 10 rising edge
+    while True: # Run forever
+        if GPIO.input(37) == GPIO.HIGH:
+            print("Button 37 was pushed!")
+    # GPIO.add_event_detect(37,GPIO.RISING,callback=button_callback) # Setup event on pin 10 rising edge
 
-    input("Ready!\n")
+    # input("Ready!\n")
 except Exception:
     print("Error with %s" % path)      
 finally:
