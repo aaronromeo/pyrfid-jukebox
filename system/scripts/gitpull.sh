5c2bf6fd1d82fb26be489442bb108619fb1803f5
Aaron Romeo <aaron@aaronromeo.com>
2023-11-17 20:21:35 -0500

    Script to pull new code from git

---
+++ system/scripts/gitpull.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -euo pipefail
+
+
+cd /home/pi/workspace/pyrfid-jukebox
+sudo -u pi git fetch
+
+if [ $(git rev-parse HEAD) != $(git rev-parse @{u}) ]; then
+    echo "New version available. Updating..."
+    sudo -u pi git pull
+
+    echo "Installing requirements"
+    sudo -u pi pip3 install -r requirements.txt
+
+    echo "Running setup"
+    bash setup.sh
+
+    echo "Restarting pyrfid_jukebox"
+    supervisord restart "pyrfid_jukebox"
+else
+    echo "No updates found."
+fi
