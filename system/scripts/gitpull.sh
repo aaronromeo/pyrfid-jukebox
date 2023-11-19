b9213ba8a59df6da0aefe2062c74410312c1ea93
Aaron Romeo <aaron@aaronromeo.com>
2023-11-18 21:17:58 -0500

    Only installing requirements if there is a change.

--- system/scripts/gitpull.sh
+++ system/scripts/gitpull.sh
@@ -7,12 +7,20 @@ echo "Checking for updates at $(date '+%Y-%m-%d %H:%M:%S')..."
 cd /home/pi/workspace/pyrfid-jukebox
 sudo -u pi git fetch
 
+pipinstall=false
+if [ $(git log -n 1 --pretty=format:"%H" -- requirements.txt) != $(git log -n 1 --pretty=format:"%H" origin/main -- requirements.txt) ]; then
+    echo "New requirements available."
+    pipinstall=true
+fi
+
 if [ $(git rev-parse HEAD) != $(git rev-parse @{u}) ]; then
     echo "New version available. Updating..."
     sudo -u pi git pull
 
-    echo "Installing requirements"
-    sudo -u pi pip3 install -r requirements.txt
+    if [ "$pipinstall" = true ]; then
+        echo "Installing requirements"
+        sudo -u pi pip3 install -r requirements.txt
+    fi
 
     echo "Running setup"
     bash setup.sh
