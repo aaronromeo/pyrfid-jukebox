defae2814cf3e5cfe95b911d4aa764f5c2e9f835
Aaron Romeo <aaron@aaronromeo.com>
2023-11-22 21:07:46 -0500

    Additional debugging logs so that supervisord doesn't think the script has exited too early.

--- system/scripts/cmus.sh
+++ system/scripts/cmus.sh
@@ -3,5 +3,9 @@
 set -xeuo pipefail
 
 if ! screen -list | grep -q "cmus"; then
-  /usr/bin/screen -dmS cmus /usr/bin/cmus
+    echo "Starting cmus..."
+    /usr/bin/screen -dmS cmus /usr/bin/cmus
+else
+    echo "cmus already running."
+    sleep 60 * 15
 fi
