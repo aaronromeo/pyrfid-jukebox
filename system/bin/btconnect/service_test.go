37d10e64a633ecfbb6792aa9f5dbd825e931e1ba
Aaron Romeo <aaron@aaronromeo.com>
2024-01-01 09:29:41 -0500

    Go unit tests

--- system/bin/btconnect/service_test.go
+++ system/bin/btconnect/service_test.go
@@ -2,6 +2,7 @@ package btconnect
 
 import (
 	"os"
+	"path/filepath"
 	"testing"
 )
 
@@ -47,4 +48,64 @@ func TestFilesAreDifferent(t *testing.T) {
 	// Cleanup
 	os.Remove("test1.txt")
 	os.Remove("test2.txt")
+}
+
+func TestHasALSAConfigChanged(t *testing.T) {
+	service := &Service{}
+
+	// Setup test files
+	testDir, err := os.MkdirTemp(".", "test_data")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer os.RemoveAll(testDir)
+	err = os.MkdirAll(filepath.Join(testDir, "system", "home"), 0755)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	testSystemConfig := filepath.Join(testDir, "system.asoundrc")
+	testRepoConfig := filepath.Join(filepath.Join(testDir, "system", "home"), ".asoundrc")
+
+	os.Setenv("PJ_ALSA_CONFIG", testSystemConfig)
+	os.Setenv("PJ_PROJECT_ROOT", testDir)
+
+	// Test 1: Identical Files
+	err = os.WriteFile(testSystemConfig, []byte("test config"), 0644)
+	if err != nil {
+		t.Fatal(err)
+	}
+	err = os.WriteFile(testRepoConfig, []byte("test config"), 0644)
+	if err != nil {
+		t.Fatal(err)
+	}
+	diff, err := service.hasALSAConfigChanged()
+	if err != nil {
+		t.Fatal(err)
+	}
+	if diff {
+		t.Errorf("Expected ALSA configs to be identical, but they were reported as different")
+	}
+
+	// Test 2: Different Files
+	err = os.WriteFile(testRepoConfig, []byte("different config"), 0644)
+	if err != nil {
+		t.Fatal(err)
+	}
+	diff, err = service.hasALSAConfigChanged()
+	if err != nil {
+		t.Fatal(err)
+	}
+	if !diff {
+		t.Errorf("Expected ALSA configs to be different, but they were reported as identical")
+	}
+
+	// Test 3: File Does Not Exist
+	os.Remove(testRepoConfig)
+	_, err = service.hasALSAConfigChanged()
+	if err == nil {
+		t.Errorf("Expected error for missing file, but got none")
+	}
+
+	// Optionally, add more tests for scenarios with environment variables set
 }
