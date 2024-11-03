b242b221353aad7bc481cb4783f1f76547e01721
Aaron Romeo <1504710+aaronromeo@users.noreply.github.com>
2024-11-03 08:16:43 -0500

    Apparently cmus uses an `rc` file for overrides. (#10)

--- system/go-pyrfid-juke-support/templategen/testdata/baselines/happypath/runner.sh
+++ system/go-pyrfid-juke-support/templategen/testdata/baselines/happypath/runner.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 mkdir -p /home/pi/.config/cmus
-sudo mv <TEMPDIR>/config-cmus-autosave.txt /home/pi/.config/cmus/autosave
-sudo chown pi /home/pi/.config/cmus/autosave
+sudo mv <TEMPDIR>/config-cmus-rc.txt /home/pi/.config/cmus/rc
+sudo chown pi /home/pi/.config/cmus/rc
 sudo supervisorctl restart cmus_manager
 mkdir -p /home/pi
 sudo mv <TEMPDIR>/asoundrc.txt /home/pi/.asoundrc
