99d689c658f3dc76a3d32d6f57e54d8882c4d4e0
Aaron Romeo <1504710+aaronromeo@users.noreply.github.com>
2024-11-03 08:06:26 -0500

    Removing the ALSA restart from btconnect (#9)

--- system/go-pyrfid-juke-support/btconnect/service.go
+++ system/go-pyrfid-juke-support/btconnect/service.go
@@ -7,7 +7,6 @@ import (
 	"log/slog"
 	"os"
 	"os/exec"
-	"path/filepath"
 	"strings"
 	"time"
 )
@@ -49,12 +48,6 @@ func (bt *Service) Run() error {
 		device := os.Getenv("PJ_BLUETOOTH_DEVICE")
 		if device == "" {
 			return fmt.Errorf("env var PJ_BLUETOOTH_DEVICE not set")
-		}
-
-		err := bt.updateALSAConfig()
-		if err != nil {
-			bt.logger.Error("updating ALSA falure", "error", err)
-			return err
 		}
 
 		count, err := bt.getBluetoothConnectionCount(device)
@@ -110,90 +103,4 @@ func (*Service) countConnectedLines(output string) int {
 		}
 	}
 	return count
-}
-
-func (bt *Service) updateALSAConfig() error {
-	if hasChanged, errHC := bt.hasALSAConfigChanged(); errHC != nil {
-		return errHC
-	} else if hasChanged {
-		bt.logger.Info("ALSA config has changed. Copying over system config...")
-
-		aslaConfig := bt.getALSASystemConfig()
-		projectAslaConfig := bt.getALSARepoConfig()
-
-		if err := bt.copyFile(aslaConfig, projectAslaConfig); err != nil {
-			bt.logger.Error(
-				"copy file error",
-				"error", err,
-				"aslaConfig", aslaConfig,
-				"projectAslaConfig", projectAslaConfig,
-			)
-			return err
-		}
-
-		if err := bt.cmdExecutor.Command("sudo", "alsactl", "restore").Run(); err != nil {
-			bt.logger.Error(
-				"alsactl restore error",
-				"error", err,
-			)
-			return err
-		}
-	}
-	return nil
-}
-
-func (bt *Service) hasALSAConfigChanged() (bool, error) {
-	aslaConfig := bt.getALSASystemConfig()
-	projectAslaConfig := bt.getALSARepoConfig()
-
-	var diff bool
-	var err error
-	if diff, err = bt.filesAreDifferent(aslaConfig, projectAslaConfig); err != nil {
-		return false, err
-	}
-	return diff, nil
-}
-
-func (*Service) copyFile(src, dst string) error {
-	input, err := os.ReadFile(src)
-	if err != nil {
-		return err
-	}
-
-	err = os.WriteFile(dst, input, 0600)
-	if err != nil {
-		return err
-	}
-
-	return nil
-}
-
-func (*Service) getALSASystemConfig() string {
-	aslaConfig := os.Getenv("PJ_ALSA_CONFIG")
-	if aslaConfig == "" {
-		aslaConfig = DefaultASLAConfig
-	}
-	return aslaConfig
-}
-
-func (*Service) getALSARepoConfig() string {
-	projectRoot := os.Getenv("PJ_PROJECT_ROOT")
-	if projectRoot == "" {
-		projectRoot = DefaultProjectRoot
-	}
-	return filepath.Join(projectRoot, DefaultRelativeASLAConfig)
-}
-
-func (*Service) filesAreDifferent(file1, file2 string) (bool, error) {
-	bytes1, err := os.ReadFile(file1)
-	if err != nil {
-		return false, err
-	}
-
-	bytes2, err := os.ReadFile(file2)
-	if err != nil {
-		return false, err
-	}
-
-	return !bytes.Equal(bytes1, bytes2), nil
 }
--- system/go-pyrfid-juke-support/templategen/testdata/baselines/matchsomeservices/runner.sh
+++ system/go-pyrfid-juke-support/templategen/testdata/baselines/matchsomeservices/runner.sh
@@ -3,3 +3,4 @@
 sudo mv <TEMPDIR>/asoundrc.txt ./testdata/destination/matchsomeservices/pi/.asoundrc
 sudo chown pi ./testdata/destination/matchsomeservices/pi/.asoundrc
 sudo supervisorctl restart btconnect
+sudo alsactl restore
